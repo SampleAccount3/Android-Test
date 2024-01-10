183fd1cd268c4debd5ab429a74004651a2ca0206
SeiKurobara <seijinkurobara@gmail.com>
2024-01-10 21:33:54 +0800

    Done with the Edit Text and Buttons

--- app/src/main/java/com/example/android_studio_test/MainActivity.java
+++ app/src/main/java/com/example/android_studio_test/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.android_studio_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
