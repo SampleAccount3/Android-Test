33a41413d7c451f4cb7955a9a95a560e2e90b635
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-01-10 13:33:36 +0800

    Done

--- app/src/main/java/com/example/android_studio_test/Activity3.java
+++ app/src/main/java/com/example/android_studio_test/Activity3.java
@@ -1,12 +1,32 @@
 package com.example.android_studio_test;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity3 extends AppCompatActivity {
+
+    private TextView tvSharedPref1,tvSharedPref2;
+    String str1,str2;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sharedpref);
+
+        tvSharedPref1 = findViewById(R.id.tvSharedPref1);
+        tvSharedPref2 = findViewById(R.id.tvSharedPref2);
+
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("SamplePref", Context.MODE_PRIVATE);
+        str1 = sp.getString("ValueA","");
+        str2 = sp.getString("ValueB","");
+
+        tvSharedPref1.setText(str1);
+        tvSharedPref2.setText(str2);
+
+
+
+
     }
 }
--- app/src/main/java/com/example/android_studio_test/MainActivity.java
+++ app/src/main/java/com/example/android_studio_test/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android_studio_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -11,26 +13,37 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button btnExplicit,btnImplicit,btnSearch,btnGetValuesAct2;
-    private EditText edSearch,edValueA,edValueB;
+    private Button btnExplicit,btnImplicit,btnSearch,btnGetValuesAct2,btnSave,btnNxtPage;
+    private EditText edSearch,edValueA,edValueB,edSharedPrefA,edSharedPrefB;
+    SharedPreferences sp;
+    String strSharedPrefA,strSharedPrefB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edSearch = findViewById(R.id.edSearch);
-        btnSearch = findViewById(R.id.btnSearch);
-//        btnExplicit = findViewById(R.id.btnExplicit);
-        btnImplicit = findViewById(R.id.btnImplicit);
-        btnGetValuesAct2 = findViewById(R.id.btnGetValues);
 
+        edSharedPrefA = findViewById(R.id.edSharedpref1);
+        edSharedPrefB = findViewById(R.id.edSharedpref2);
+        edSearch = findViewById(R.id.edSearch);
         edValueA = findViewById(R.id.edValueA);
         edValueB = findViewById(R.id.edValueB);
+
+        btnImplicit = findViewById(R.id.btnImplicit);
+        btnGetValuesAct2 = findViewById(R.id.btnGetValues);
+        btnSave = findViewById(R.id.btnSharedPrefSave);
+        btnNxtPage = findViewById(R.id.btnNxtPage);
+        btnSearch = findViewById(R.id.btnSearch);
+//        btnExplicit = findViewById(R.id.btnExplicit);
 
         btnSearch.setOnClickListener(this);
 //        btnExplicit.setOnClickListener(this);
         btnImplicit.setOnClickListener(this);
         btnGetValuesAct2.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
+        btnNxtPage.setOnClickListener(this);
+
+        sp = getSharedPreferences("SamplePref", Context.MODE_PRIVATE);
 
 
     }
@@ -59,6 +72,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             i.putExtra("ValueB",valB);
 
             startActivity(i);
+        }
+
+        if (R.id.btnSharedPrefSave == view.getId()){
+            strSharedPrefA = edSharedPrefA.getText().toString();
+            strSharedPrefB = edSharedPrefB.getText().toString();
+
+            SharedPreferences.Editor editor = sp.edit();
+
+            editor.putString("ValueA",strSharedPrefA);
+            editor.putString("ValueB",strSharedPrefB);
+            editor.commit();
+
+            Toast.makeText(this, "Shared Pref Save Success",Toast.LENGTH_SHORT).show();
+
+        }
+        if (R.id.btnNxtPage == view.getId()){
+            Intent intent = new Intent(MainActivity.this,Activity3.class);
+
+            startActivity(intent);
+
         }
     }
 }
